c562ef6f0b12e43dd14d75e413553d5e49eca70d
Ubuntu <ubuntu@ip-172-31-32-137.ec2.internal>
2014-05-27 08:10:42 +0000

    Initial commit.

--- notebooks/weather.mapreduce/mr_pca_01.py
+++ notebooks/weather.mapreduce/mr_pca_01.py
@@ -1,95 +1,62 @@
 #!/usr/bin/python
 """
-collect the statistics for each station.
+PCA
 """
-import re,pickle,base64,zlib
-from sys import stderr
 import sys
-
-sys.path.append('/usr/lib/python2.6/dist-packages') # a hack because anaconda made mrjob unreachable
+import numpy as np
+sys.path.append('/usr/lib/python2.6/dist-packages')
 from mrjob.job import MRJob
-from mrjob.protocol import *
-
-import traceback
-from functools import wraps
+import re
 from sys import stderr
-
-"""this decorator is intended for decorating a function, not a
-generator.  Therefor to use it in the context of mrjob, the generator
-should call a function that handles a single input records, and that
-function should be decorated.
-
-The reason is that if a generator throws an exception it exits and
-cannot process any more records.
-
-"""
-def ECatch(func):
-    f_name=func.__name__
-    @wraps(func)
-    def inner(self,*args,**kwargs):
-        try:
-            self.increment_counter(self.__class__.__name__,'total in '+f_name,1)
-            return func(self,*args,**kwargs)
-        except Exception as e:
-            self.increment_counter(self.__class__.__name__,'errors in '+f_name,1)
-            stderr.write('Error:')
-            stderr.write(str(e))
-            traceback.print_exc(file=stderr)
-            stderr.write('Arguments were %s, %s\n'%(args,kwargs))
-            pass
-    return inner        
-
-"""
-Functions for encoding and decoding arbitrary object into ascii 
-so that they can be passed through the hadoop streaming interface.
-"""
-
-def loads(eVal):
-    """ Decode a string into a value """
-    return pickle.loads(zlib.decompress(base64.b64decode(eVal)))
-
-def dumps(Value):
-    """ Encode a value as a string """
-    return base64.b64encode(zlib.compress(pickle.dumps(Value),9))
+import pickle
 
 class MRPCA(MRJob):
+    def configure_options(self):
+        super(MRPCA,self).configure_options()
+        self.add_file_option('--buckets')
 
-    @ECatch
-    def map_one(self,line):
-        return line.split(',')
+    def reducer_init(self):
+        f = gzip.open( self.options.buckets, "rb" )
+        pickleFile = cPickle.Unpickler( f )
+        self.buckets = pickleFile.load()#[['latitude','longitude','elevation']]
+        f.close()
     
-    def mapper(self, _, line):    
-        elements=self.map_one(line)
-        #if elements[1] == "TMAX" or elements[1] == "TMIN":
-        #    yield(elements[0], elements[1:])
-        yield(_, elements)
+    def mapper_concatenate(self, _, line):
+        #buckets = pickle.load( open( "buck/buckets.pkl", "rb" ) )
+        try:
+            self.increment_counter('MrJob Counters','mapper-all',1)
+            elements=line.split(',')
+            number_defined=sum([e!='' for e in elements[3:]])
+            if elements[0] != 'station' and number_defined == 365 and elements[1] in ['TMAX', 'TMIN']:
+                yield ([elements[0],elements[2]],[1]+elements[:])
+
+        except Exception, e:
+            stderr.write('Error in line:\n'+line)
+            stderr.write(e)
+            self.increment_counter('MrJob Counters','mapper-error',1)            
+
+    def reducer_concatenate(self, station_year, counts):
+        #buckets = pickle.load( open( "buck/buckets.pkl", "rb" ) )
+        self.increment_counter('MrJob Counters','reducer',1)
+        new=[]
+        for v in counts:
+            if v[2]=='TMAX':
+                new=v[4:]+new
+            else:
+                new=new+v[4:]
+        tmp='1'
+        if len(new)==730:
+            for s in new:
+                tmp=tmp+' '+s
+            tmp=tmp+' 1'
+            yield (self.buckets[station_year[0]],[tmp])
             
-    def check_integrity(self,meas,year,length):
-        year=int(year)
-        if year<1000 or year > 2014: return False
-        if meas=='': return False
-        if length != 367: return False
-        return True
-    
-    @ECatch
-    def reduce_one(self,S,vector):
-        meas=vector[0]
-        year=vector[1]
-        length=len(vector)
-        number_defined=sum([e!='' for e in vector[2:]])
-        assert self.check_integrity(meas,year,length)==True
-        S[(meas,int(year))]=number_defined
-    
-    def reducer(self, _, elements):
-        if elements[1] == "TMAX" or elements[1] == "TMIN":
-            yield(elements[0], elements[1:])
-    
-    #def reducer(self, station, vectors):
-        #S={}
-        #for vector in vectors:
-        #    self.reduce_one(S,vector)
-        #yield(station,dumps(S))
-        #yield(station, vectors)
-                              
+    def steps(self):
+        return [
+            self.mr(mapper=self.mapper_concatenate,
+                    reducer=self.reducer_concatenate)#,
+            #self.mr(reducer=self.reducer_find_max_word)
+        ]    
+
 if __name__ == '__main__':
     MRPCA.run()
